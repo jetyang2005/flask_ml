abfde33f4ee4b917035b00c318882254f1810334
yangwm <286388651@qq.com>
2017-12-28 19:43:46 +0800

    初次上传

--- app/config.py
+++ app/config.py
@@ -1,3 +1,11 @@
+# -*- coding:utf-8 -*-
+
+# 配置当前应用端口号及IP地址
+DEBUG = True
+PORT = 3333
+HOST = "10.0.0.24"
+
+# 关系型数据库配置，此数据库配置需要与springboot平台数据库保持一致
 DB_USER = 'root'
 DB_PASSWORD = '123456'
 DB_HOST = '118.190.132.107'
@@ -5,16 +13,20 @@ DB_DB = 'linkdata20171030'
 DB_PORT = 3306
 DB_CHAR = "utf8"
 
-DEBUG = True
-PORT = 3333
-HOST = "10.0.0.11"
 
+# SpringBoot平台Token秘钥
 SECRET_KEY = "adsfialle323klsflADASF"
 
+# Elasticsearch配置信息
 ES_HOST = '118.190.132.107'
 ES_PORT = 9200
 
+# Kafka配置信息，可以在python中向kafka发送内容
 KAFKA_HOST_PORT = '118.190.132.107:9092'
 
+# SQLALCHEMY配置路径，此路径无需修改
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = 'mysql://' + DB_USER + ':' + DB_PASSWORD + '@' + DB_HOST + '/' + DB_DB
+
+# elasticsearch基础类库日志
+ELASTICSEARCH_LOG_DIR = 'log/elastticsearch.log'
